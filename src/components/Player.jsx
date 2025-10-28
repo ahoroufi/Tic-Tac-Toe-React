0b3fa7cf625bd64ca3f89e3a978d759281e6b808
Arash Horoufi <6829077+ahoroufi@users.noreply.github.com>
2025-10-28 16:26:50 -0700

    96. Final Polishing

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
 
-function Player({initialName, symbol, isActive}) {
+function Player({initialName, symbol, isActive, onChangeName}) {
     const [playerName, setPlayerName] = useState(initialName);
     const [ isEditing, setIsEditing] = useState(false);
+
     function handleEditClick(){
         setIsEditing((editing) => !editing); // => schedule a state update to true
+        if(isEditing) {
+            onChangeName(symbol, playerName);
+        }
     }
 
     function handleChange(event) {  
         setPlayerName(event.target.value);
+
     }
     let editablePlayerName = <span className='player-name'>{playerName}</span>
     if (isEditing) {
